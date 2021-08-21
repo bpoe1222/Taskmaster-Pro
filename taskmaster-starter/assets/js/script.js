e1606f48d1bdb8027ccacd0259b428f79fd76c25
Bailey Poe <baileypoe1222@gmail.com>
2021-08-21 17:10:52 -0500

    added moment.js functionality

--- taskmaster-starter/assets/js/script.js
+++ taskmaster-starter/assets/js/script.js
@@ -1,4 +1,18 @@
 var tasks = {};
+var auditTask = function(taskEl) {
+  var date = $(taskEl).find("span").text().trim();
+  var time = moment(date, "L").set("hour", 17);
+
+  $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
+
+  if (moment().isAfter(time)) {
+    $(taskEl).addClass("list-group-item-danger");
+  }
+  else if (Math.abs(moment().diff(time, "days")) <= 2) {
+    $(taskEl).addClass("list-group-item-warning")
+  }
+};
+
 
 var createTask = function(taskText, taskDate, taskList) {
   // create elements that make up a task item
@@ -12,6 +26,9 @@ var createTask = function(taskText, taskDate, taskList) {
 
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
+
+  // Check Due Date
+  auditTask(taskLi);
 
 
   // append to ul list on the page
@@ -115,18 +132,22 @@ $(".list-group").on("blur", "textarea", function() {
 });
 
 $(".list-group").on("click", "span", function() {
-  var date = $(this)
-  .text()
-  .trim();
+  var date = $(this).text().trim();
   var dateInput = $("<input>")
   .attr("type", "text")
   .addClass("form-control")
   .val(date);
   $(this).replaceWith(dateInput);
+  dateInput.datepicker({
+    minDate: 1,
+    onClose: function() {
+      $(this).trigger("change");
+    }
+  });
   dateInput.trigger("focus");
 });
 
-$(".list-group").on("blur", "input[type='text']", function() {
+$(".list-group").on("change", "input[type='text']", function() {
   var date= $(this)
   .val()
   .trim();
@@ -143,6 +164,7 @@ $(".list-group").on("blur", "input[type='text']", function() {
   .addClass("badge badge-primary badge-pill")
   .text(date);
   $(this).replaceWith(taskSpan);
+  auditTask($(taskSpan).closest(".list-group-item"))
 });
 
 // remove all tasks
@@ -213,3 +235,8 @@ $("#trash").droppable({
     console.log("out");
   }
 });
+
+$("#modalDueDate").datepicker({
+  minDate: 1
+});
+
